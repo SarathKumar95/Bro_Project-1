ab2a2efc56a1d37a4329d747e0ede35ae274b893
Sarath Kumar <worksarath95@gmail.comgit config --global user.email worksarath95@gmail.comgit config --global user.email worksarath95@gmail.com>
2022-11-04 23:40:30 +0530

    Custom user created

---
+++ ipocket/accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from accounts.managers import UserManager
+
+
+class MyUser(AbstractUser):
+    username = None
+    email = models.CharField(max_length=50, unique=True)
+    mobile_number = models.CharField(max_length=15)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
