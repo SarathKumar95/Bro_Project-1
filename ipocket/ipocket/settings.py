ca290a6ed604238b6c3629fcce5fc8a7e505cfe5
Sarath Kumar <worksarath95@gmail.com>
2022-11-09 13:39:49 +0530

    Product app started

--- ipocket/ipocket/settings.py
+++ ipocket/ipocket/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'phonenumber_field'
+    'phonenumber_field',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# media files here
 
+MEDIA_URL = 'images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
