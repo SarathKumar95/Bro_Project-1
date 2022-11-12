4943d95e62750723d3ad9a5691b4c31190de7e6a
Sarath Kumar <worksarath95@gmail.com>
2022-11-12 13:36:13 +0530

    confirm set with block button

--- ipocket/accounts/static/js/owner/owner.js
+++ ipocket/accounts/static/js/owner/owner.js
@@ -1,7 +1,12 @@
 function blockUser(){
-    confirmAction = confirm("Are you sure you want to block this user? ");
+    let confirmAction = confirm("Are you sure you want to block this user? ");
     if(confirmAction){
-        
+        console.log("Inside confirm");
+        let url = $("#blockbtn").attr("data-url");
+        window.location = url;
+    }
+    else{
+        console.log("Do nothing!");
     }
 
 
