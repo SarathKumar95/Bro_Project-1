68995bd4e644e39b8159259e9d9a4d1f4a83b26c
Sarath Kumar <worksarath95@gmail.com>
2022-11-15 14:11:06 +0530

    new auth added

--- ipocket/accounts/forms.py
+++ ipocket/accounts/forms.py
@@ -1,50 +1,36 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms 
-from django.forms import ModelForm,TextInput,NumberInput,Select,Textarea,FileInput
+from django import forms
+from django.forms import ModelForm, TextInput
 from .models import MyUser
 from phonenumber_field.formfields import PhoneNumberField
-
+from phonenumber_field.widgets import PhoneNumber
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={
-        'placeholder' : 'First Name',
-        'style' : 'width: 300px; height:30px;',
-        'class': 'form-control'
-    }))
-    last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={
-        'placeholder' : 'Last Name',
-        'style' : 'width: 300px; height:30px;',
-        'class': 'form-control'
-    }))
+    first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'style': 'width: 300px; height:30px;',
+                                                                                             'class': 'form-control'
+                                                                                             }))
+    last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'style': 'width: 300px; height:30px;',
+                                                                                            'class': 'form-control'
+                                                                                            }))
 
-    mobile_number = PhoneNumberField(region='IN', widget=forms.NumberInput(attrs={
-        'placeholder' : 'Mobile Number',
-        'style' : 'width: 300px; height:30px;',
-        'class': 'form-control'
-    })
-     )
+    mobile_number = PhoneNumberField(region='IN') 
 
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'placeholder' : 'Email',
-        'style' : 'width: 300px; height:30px;',
-        'class': 'form-control'
-    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'style': 'width: 300px; height:30px;',
+                                                            'class': 'form-control'
+                                                            }))
 
     password1 = forms.CharField(widget=forms.PasswordInput(
         attrs={
-            'placeholder' : 'Password',
-            'style' : 'width: 300px; height:30px;',
+            'style': 'width: 300px; height:30px;',
             'class': 'form-control'
 
         }
     ))
 
-    
     password2 = forms.CharField(widget=forms.PasswordInput(
         attrs={
-            'placeholder' : 'Confirm Password',
-            'style' : 'width: 300px; height:30px;',
+            'style': 'width: 300px; height:30px;',
             'class': 'form-control'
 
         }
